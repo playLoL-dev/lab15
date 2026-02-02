0c3ea1a182458b60c62a226dc2f0b90bca2da70e
playLoL <playza.1987@gmail.com>
2026-02-02 22:29:32 +0700

    lab15_1.cpp completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,31 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	
-	
+        int a = 5;
+		char b = 'A';
+        char &c = b;
+		void *x = &a;
+		void *y = &b;
+		void *z = &c;
+
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+		cout << &a << " " << (void*)y << " " << (void*)z << " " << &x << " " << &y << " " << &z << endl;
+
+		*(char*)y = 'F';
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+		*(char*)z = 'W';
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+		*(int*)x = 6;
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+
+		*(int*)x = 7;
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+		
 	return 0;
+
 }
