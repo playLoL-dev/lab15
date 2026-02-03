82b7976dadc83385d83d3c1cfaf7a5ae94365d82
playLoL <playza.1987@gmail.com>
2026-02-03 13:56:25 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -18,4 +18,16 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int arr[4] = {a,b,c,d};
+	for(int i = 0 ; i < 4 ; i++){
+		int r = rand() % 4;
+		int temp = arr[i];
+		arr[i] = arr[r];
+		arr[r] = temp;
+	}
+	a = arr[0];
+	b = arr[1];
+	c = arr[2];
+	d = arr[3];
+}
